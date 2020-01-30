c571837f31107afc387daa01f208d947e1142ce9
Futuremappermydud <54294576+owenski08@users.noreply.github.com>
2020-01-30 01:22:51 -0500

    quick fix

--- UI/Controllers/ProfilePageController.cs
+++ UI/Controllers/ProfilePageController.cs
@@ -5,8 +5,8 @@ namespace FightSabers.UI.Controllers
 {
     internal class ProfilePageController : FightSabersViewController
     {
-        public override string ResourceName => "FightSabers.UI.Views.QuestPickerPageView.bsml";
-        public override string ContentFilePath => "C:/Users/Owens/Documents/GitHub/FightSabershop/FightSabers/FightSabers/UI/Views/QuestPickerPageView.bsml";
+        public override string ResourceName => "FightSabers.UI.Views.ProfilePageView.bsml";
+        public override string ContentFilePath => "C:/Users/Owens/Documents/GitHub/FightSabershop/FightSabers/FightSabers/UI/Views/ProfilePageView.bsml";
 
         [UIParams]
         private BSMLParserParams parserParams;
